596e58c333e5a98c7e1a6af7688b87e04b768025
Shahtr1 <tramboos123@gmail.com>
2023-10-17 22:40:54 +0530

    finished all mds

--- Java-and-Spring-Docs-main/JDBC.java
+++ Java-and-Spring-Docs-main/JDBC.java
@@ -18,73 +18,45 @@ import javax.servlet.http.HttpServletResponse;
 public class TestDbServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public TestDbServlet() {
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * Default constructor.
+	 */
+	public TestDbServlet() {
+	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		// setup connection variables
 		String user = "springsstudent";
 		String pass = "springstudent";
-		
+
 		String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false";
 		String driver = "com.mysql.jdbc.Driver";
-		
+
 		// get connection to database
 		try {
 			PrintWriter out = response.getWriter();
-			
+
 			out.println("Connecting to database: " + jdbcUrl);
-			
+
 			Class.forName(driver);
-			
+
 			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
-			
+
 			out.println("SUCCESS!!");
-			
+
 			myConn.close();
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new ServletException(e);
 		}
-		
+
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
