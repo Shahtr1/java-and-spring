02c94512dd015bac4529118104acf742d6bbf89b
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2024-02-01 15:39:20 +0530

    Finished microservices

--- Microservices/QuizService/src/main/java/com/quiz/services/QuestionClient.java
+++ Microservices/QuizService/src/main/java/com/quiz/services/QuestionClient.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(url = "http://localhost:8082",value = "Question-Client")
+//@FeignClient(url = "http://localhost:8082",value = "Question-Client")
+@FeignClient(name = "QUESTION-SERVICE")
 public interface QuestionClient {
     @GetMapping("/question/quiz/{quizId}")
     List<Question> getQuestionOfQuiz(@PathVariable Long quizId);
