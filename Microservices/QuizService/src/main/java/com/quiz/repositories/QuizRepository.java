161bb049ab04222f426475de82a40da4c7caf241
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2024-02-24 20:41:58 +0530

    Refactoring

--- Microservices/QuizService/src/main/java/com/quiz/repositories/QuizRepository.java
+++
@@ -1,7 +0,0 @@
-package com.quiz.repositories;
-
-import com.quiz.entities.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizRepository extends JpaRepository<Quiz, Long> {
-}
