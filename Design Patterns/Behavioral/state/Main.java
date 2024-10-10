29fec78ea26a4001fa44937dd9a87bc5bf46fd15
Shahtr1 <tramboos123@gmail.com>
2024-10-10 22:04:10 +0530

    added state pattern

---
+++ Design Patterns/Behavioral/state/Main.java
@@ -0,0 +1,20 @@
+package Behavioral.state;
+
+public class Main {
+    public static void main(String[] args) {
+        Context context = new Context();
+
+        StartState startState = new StartState();
+        startState.doAction(context);
+
+        System.out.println(context.getState().toString());
+
+        StopState stopState = new StopState();
+        stopState.doAction(context);
+
+        System.out.println(context.getState().toString());
+
+
+    }
+
+}
---
+++ Design Patterns/Behavioral/state/State.java
@@ -0,0 +1,5 @@
+package Behavioral.state;
+
+public interface State {
+    public void doAction(Context context);
+}
